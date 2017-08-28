80c571f20c86134dce457bc86f29087e853e139c
David Schroh <dschroh@uncharted.software>
2017-08-28 14:24:12 -0400

    build tweaks

--- deploy/swarm_deploy.sh
+++ deploy/swarm_deploy.sh
@@ -3,6 +3,7 @@ USER=$1
 PASS=$2
 BUILD=$3
 STACK=$4
+REPO=https://docker.uncharted.software
 
-docker login docker.uncharted.software -u=$1 -p=$1
+echo Deploy stack ${BUILD}-docker-compose.yml as ${STACK}  
 docker stack deploy --compose-file /tmp/${BUILD}-docker-compose.yml --with-registry-auth=true ${STACK}
