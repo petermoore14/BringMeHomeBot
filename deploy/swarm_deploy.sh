cee79f6011d776dc091b8f573ad392bfcd6f4e37
David Schroh <dschroh@uncharted.software>
2017-08-28 16:05:39 -0400

    undo AWS

--- deploy/swarm_deploy.sh
+++ deploy/swarm_deploy.sh
@@ -1,15 +1,12 @@
 
 DOCKER_REPO=$1
-AWS_ACCESS_KEY_ID=$2
-AWS_SECRET_ACCESS_KEY=$3
+DOCKER_USER=$2
+DOCKER_PASS=$3
 BUILD=$4
 STACK=$5
 
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-
 echo Login into ${DOCKER_REPO}
-`aws ecr get-login --no-include-email --region us-east-1`
+docker login ${DOCKER_REPO} --username ${DOCKER_USER} --password ${DOCKER_PASS}
 
 echo Deploy stack ${BUILD}-docker-compose.yml as ${STACK}
 cd /tmp
