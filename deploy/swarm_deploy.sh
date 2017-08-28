40ffeab7eea50e87ea0a700c666dedf836e997b9
David Schroh <dschroh@uncharted.software>
2017-08-28 15:07:01 -0400

    Add login back to swarm

--- deploy/swarm_deploy.sh
+++ deploy/swarm_deploy.sh
@@ -1,6 +1,12 @@
 
-BUILD=$1
-STACK=$2
+DOCKER_REPO=$1
+DOCKER_USER=$2
+DOCKER_PASS=$3
+BUILD=$4
+STACK=$5
+
+echo Login into ${DOCKER_REPO}
+docker login ${DOCKER_REPO} --username "${DOCKER_USER}" --password "${DOCKER_PASS}"
 
 echo Deploy stack ${BUILD}-docker-compose.yml as ${STACK}
 cd /tmp
